794f470a2b89beaa2db847e25b178b81149d66c6
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2021-07-09 09:42:31 +0800

    Created sign-in and sign-up page

--- src/App.js
+++ src/App.js
@@ -3,8 +3,11 @@ import {Route, Switch} from 'react-router-dom';
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import ShopPage from './pages/shop/shop.component';
+
 import Header from './components/header/header.component';
+
 
 function App() {
   return (
@@ -13,6 +16,7 @@ function App() {
       <Switch>
         <Route exact path="/" component={HomePage}></Route>
         <Route exact path="/shop" component={ShopPage}></Route>
+        <Route exact path="/signin" component={SignInAndSignUpPage}></Route>
       </Switch>
     </div>
   );
