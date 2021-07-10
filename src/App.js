23a0f8dfde2ed6b9a951a82a375108dec5f7aabd
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2021-07-10 19:40:45 +0800

    Added sign up function for email and password.

--- src/App.js
+++ src/App.js
@@ -33,6 +33,8 @@ class App extends React.Component{
               id: snapShot.id,
               ...snapShot.data()
             }
+          }, () => {
+            console.log(this.state);
           });
     
         });
