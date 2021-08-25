8632d07c03f766f9e86c4783658ce733d97b56c5
Jeremy Agcaoili <agcaoili.jeremy@gmail.com>
2021-08-25 21:24:44 +0800

    Shop data and spinner

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,6 +1,4 @@
 import React from 'react';
-import {Link} from 'react-router-dom';
-import './header.style.scss';
 import {connect} from 'react-redux';
 import {ReactComponent as Logo} from '../../assets/crown.svg';
 import CartIcon from '../cart-icon/cart-icon.component';
@@ -8,33 +6,34 @@ import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 import { createStructuredSelector } from 'reselect';
 import { selectCartHidden } from '../../redux/cart/cart.selectors';
 import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { HeaderContainer, LogoContainer, OptionsContainer, OptionLink } from './header.style';
 
 import {auth} from '../../firebase/firebase.utils';
 
 const Header = ({currentUser, hidden}) => (
-  <div className="header">
-    <Link className="logo-container" to="/">
+  <HeaderContainer>
+    <LogoContainer to="/">
       <Logo className="logo" />
-    </Link>
-    <div className="options">
-      <Link className="option" to="/shop">
+    </LogoContainer>
+    <OptionsContainer>
+      <OptionLink to="/shop">
         SHOP
-      </Link>
-      <Link className="option" to="/contact">
+      </OptionLink>
+      <OptionLink to="/contact">
         CONTACT
-      </Link>
+      </OptionLink>
       {
         currentUser ?
-        (<div className="option" onClick={() => auth.signOut()}> SIGN OUT </div>)
+        (<OptionLink as="div" onClick={() => auth.signOut()}> SIGN OUT </OptionLink>)
         :
-        (<Link className="option" to="/signin"> SIGN IN </Link>)
+        (<OptionLink to="/signin"> SIGN IN </OptionLink>)
       }
       <CartIcon />
-    </div>
+    </OptionsContainer>
     {
       hidden ? null : ( <CartDropdown />)
     }
-  </div>
+  </HeaderContainer>
 );
 
 const mapStateToProps = createStructuredSelector({
